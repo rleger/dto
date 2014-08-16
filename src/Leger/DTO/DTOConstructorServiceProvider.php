a26facd83a0c7a50f9b19aa2a22ec1700f2f517a
Romain Léger <legerrom@gmail.com>
2014-08-16 22:16:39 +0200

    adding service provider

---
+++ src/Leger/DTO/DTOConstructorServiceProvider.php
@@ -0,0 +1,39 @@
+<?php namespace Leger\dto;
+
+class DTOConstructorServiceProvider extends ServiceProvider {
+
+        /**
+         * Indicates if loading of the provider is deferred.
+         *
+         * @var bool
+         */
+        protected $defer = false;
+
+        /**
+         * Booting
+         */
+        public function boot()
+        {
+            $this->package('leger/dto');
+        }
+
+        /**
+         * Register the commands
+         *
+         * @return void
+         */
+        public function register()
+        {
+        }
+
+        /**
+         * Get the services provided by the provider.
+         *
+         * @return array
+         */
+        public function provides()
+        {
+            return array();
+        }
+
+}
